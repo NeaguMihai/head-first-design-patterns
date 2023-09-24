763fb336d8d75437c6c0eee5ffba6e6203baa201
Mihai Neagu <mihai.neagu@webitfactory.io>
2023-09-25 00:58:19 +0300

    chore: linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,95 +1,95 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		project: './tsconfig.json',
-		tsconfigRootDir: __dirname,
-		sourceType: 'module',
-	},
-	plugins: [
-		'@typescript-eslint/eslint-plugin',
-		'prettier',
-		'unicorn',
-		'import',
-		'simple-import-sort',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'prettier',
+    'unicorn',
+    'import',
+    'simple-import-sort',
     'sonarjs',
-	],
-	extends: [
-		'plugin:prettier/recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  extends: [
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:import/recommended',
-		'plugin:import/recommended',
-		'plugin:unicorn/recommended',
+    'plugin:import/recommended',
+    'plugin:unicorn/recommended',
     'plugin:sonarjs/recommended',
-
-	],
-	root: true,
-	env: {
-		node: true,
-		jest: true,
-	},
-	ignorePatterns: ['.eslintrc.js'],
-	rules: {
-    "unicorn/filename-case": [
-      "error",
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'unicorn/filename-case': [
+      'error',
       {
-        "cases": {
-          "kebabCase": true,
-          "pascalCase": true
-        }
-      }
+        cases: {
+          kebabCase: true,
+          pascalCase: true,
+        },
+      },
     ],
     'unicorn/prefer-top-level-await': 'off',
     'unicorn/prevent-abbreviations': 'off',
+    'unicorn/require-array-join-separator': 'off',
     'no-array-constructor': 'error',
-		'no-mixed-operators': 'error',
-		'import/default': 'off',
-		'class-methods-use-this': 'off',
+    'no-mixed-operators': 'error',
+    'import/default': 'off',
+    'class-methods-use-this': 'off',
     'sonarjs/no-duplicate-string': 'off',
     quotes: [
-			'error',
-			'single',
-			{
-				allowTemplateLiterals: true,
-				avoidEscape: true,
-			},
-		],
-		'no-console': [
-			'error',
-			{
-				allow: ['warn', 'error', 'info','table'],
-			},
-		],
+      'error',
+      'single',
+      {
+        allowTemplateLiterals: true,
+        avoidEscape: true,
+      },
+    ],
+    'no-console': [
+      'error',
+      {
+        allow: ['warn', 'error', 'info', 'table'],
+      },
+    ],
     'prefer-const': [
-			'error',
-			{
-				destructuring: 'any',
-				ignoreReadBeforeAssign: false,
-			},
-		],
+      'error',
+      {
+        destructuring: 'any',
+        ignoreReadBeforeAssign: false,
+      },
+    ],
     'padding-line-between-statements': [
-			'error',
-			{
-				blankLine: 'always',
-				prev: ['const', 'let', 'var'],
-				next: '*',
-			},
-			{
-				blankLine: 'any',
-				prev: ['const', 'let', 'var'],
-				next: ['const', 'let', 'var'],
-			},
-		],
+      'error',
+      {
+        blankLine: 'always',
+        prev: ['const', 'let', 'var'],
+        next: '*',
+      },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
+    ],
     'newline-before-return': 'error',
-    "import/no-unresolved": 'off',
+    'import/no-unresolved': 'off',
     // clasic
-		'@typescript-eslint/no-floating-promises': 'off',
-		'@typescript-eslint/interface-name-prefix': 'off',
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-unsafe-assignment': 'off',
-		'@typescript-eslint/no-unsafe-argument': 'off',
-		'@typescript-eslint/no-unsafe-call': 'off',
-		'@typescript-eslint/no-unsafe-member-access': 'off',
-	},
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+  },
 };
